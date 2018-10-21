6d5153021d0980dac0f4b383fe9cb6504a85167e
JumbooWong <493182574@qq.com>
2018-10-21 21:09:56 +0800

    window 上 makefile的demo

---
+++ makefile/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "map.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello Makefile!" << endl;
+    run();
+    return 0;
+}
---
+++ makefile/map.h
@@ -0,0 +1,7 @@
+#ifndef MAP_H
+#define MAP_H
+
+// run
+void run();
+
+#endif
